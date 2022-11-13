b87b68539594817f55cf6b1fc739eb7ff2a8d109
VLautaroUTN <utnlautaro@gmaiol.com>
2022-11-12 21:11:44 -0300

    Creacion de funcion temperatura, preparado para empezar funcion wifi

--- NodeMCU_Sketch_1/NodeMCU_Sketch_1.ino
+++ NodeMCU_Sketch_1/NodeMCU_Sketch_1.ino
@@ -1,12 +1,61 @@
 #include <OneWire.h>
-
+#include <ESP82>
 OneWire  ds(D1);  // on pin D1 (a 4.7K resistor is necessary)
+
+
+// ----- Prototipos de funcion -----
+float medirTemperatura();
+float comunicacionWeb();
+
+
+// Variables globales
+const char* ssid = "RED";
+const char* password = "CONTRASEÑA";
+
+WiFiServer server(80);
+
+String header;
+
+unsigned long lastTime, timeout = 2000;
+const int outputPin = 0; // pin de salida
+String outputState = "apagado"; //almacenar el estado actual de la salida
+
 
 void setup(void) {
   Serial.begin(9600);
+    pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, LOW);
+  
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid,password);
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Dispositivo conectado.");
+  Serial.println("Direccion IP: ");
+  Serial.print(WiFi.localIP());
+
+  server.begin();
+
 }
 
-void loop(void) {  // Puedo definir una funcion que haga todo el control de la temperatura
+
+void loop(void) {
+ 
+}
+
+
+// ----- Implementacion de funciones -----
+
+//Mide la temperatura con el sensor DS18B20 y devuelve un float
+float medirTemperatura(){
   byte i;
   byte present = 0;
   byte data[9];
@@ -53,9 +102,94 @@ void loop(void) {  // Puedo definir una funcion que haga todo el control de la t
   //// default is 12 bit resolution, 750 ms conversion time
   
   celsius = (float)raw / 16.0;
-  Serial.print("Temperature = ");
-  Serial.print(celsius);
-  Serial.print(" ºC");
-  Serial.println(" ");
-  delay(5000);
+  return celsius;
+}
+
+// Comunica el NodeMCU 
+void comunicacionWeb(){
+
+  WiFiClient client = server.available();
+
+  if(client)
+  {
+    lastTime = millis();
+    
+    Serial.println("Nuevo cliente");
+    String currentLine = "";
+
+    while(client.connected() && millis() - lastTime <= timeout)
+    {
+
+      if(client.available())
+      {
+        
+        char c = client.read();
+        Serial.write(c);
+        header += c;
+
+        if(c == '\n')
+        {
+          
+          if(currentLine.length() == 0)
+          {
+
+            ////////// ENCABEZADO HTTP ////////////
+
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+
+            if (header.indexOf("GET /on") >= 0) {
+              Serial.println("GPIO encendido");
+              outputState = "encendido";
+              digitalWrite(outputPin, HIGH);
+            }                         
+            else if (header.indexOf("GET /off") >= 0) {
+              Serial.println("GPIO apagado");
+              outputState = "apagado";
+              digitalWrite(outputPin, LOW);
+            } 
+            
+
+             //////// PAGINA WEB //////////////
+             
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("</head>");
+
+            client.println("<body></body>");
+
+            client.println("</html>");
+
+            client.println();
+            break;
+
+            
+            /////////////////////////////////////
+          }
+          else
+          {
+            currentLine = "";
+          }
+        }
+        else if ( c != '\r')
+        {
+          currentLine += c;
+        }
+        
+        
+      }
+
+      
+    }
+
+    header = "";
+    client.stop();
+    Serial.println("Cliente desconectado.");
+    Serial.println("");
+  }
+
 }
